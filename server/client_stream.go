7e754e535c8fed8a634cf7c4ea7ad9c957a69659
samarjit-singh <Samarjitbravo@gmail.com>
2024-10-13 22:34:31 +0530

    create and call client streaming

---
+++ server/client_stream.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	pb "go-grpc/proto"
+	"io"
+	"log"
+)
+
+func (s *helloServer) SayHelloClientStreaming(stream pb.GreetService_SayHelloClientStreamingServer) error {
+	var messages []string
+	for {
+		req,err := stream.Recv()
+		if err == io.EOF{
+			return stream.SendAndClose(&pb.MessagesList{Messages:  messages})
+		}
+
+		if err != nil {
+			return err
+		}
+		log.Printf("got request with name: %v", req.Name)
+		messages = append(messages, "Hello", req.Name)
+	}
+}
