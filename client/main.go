a280fb4f410dc0659c1bedb887b07c667ae09c59
samarjit-singh <Samarjitbravo@gmail.com>
2024-10-13 16:46:33 +0530

    create and call unary api

--- client/main.go
+++ client/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	pb "go-grpc/proto"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const(
+	port = ":8000"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	// defer ensure that this function runs at the very end of execution
+	 defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	// name := &pb.NamesList{
+	// 	Names: []string{"Samar", "Alice", "Bob"},
+	// }
+
+	callSayHello(client)
+}
